1f830604a48ae067bc801c9562cf35e6901fd2ec
phantomy2kcia <phantomy2kcia@gmail.com>
2018-04-16 19:12:12 +0900

    asd

---
+++ js/main.js
@@ -0,0 +1,29 @@
+/* requestAnimationFrame shim */
+if (window.requestAnimationFrame == null) {
+    window.requestAnimationFrame =
+        window.webkitRequestAnimationFrame ||
+        window.mozRequestAnimationFrame    ||
+        function(callback){
+            window.setTimeout(callback, 1000 / 60);
+        };
+}
+
+function comma(num) {
+    return num.toString().replace(/(\d)(?=(\d{3})+$)/g, '$1,');
+}
+
+var particles = null,
+    controller = null;
+
+function updateCount() {
+    var count = particles.statesize[0] * particles.statesize[1];
+    $('.count').text(comma(count));
+}
+
+$(document).ready(function() {
+    var canvas = $('#display')[0];
+    particles = new Particles(canvas, 1024 * 16, 3).draw().start();
+    controller = new Controller(particles);
+    new FPS(particles);
+    updateCount();
+});
